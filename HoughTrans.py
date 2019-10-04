4bb310fa49d7d5dfecb01c270793e2c2ea8bce16
brunno <34986733+brunnuh@users.noreply.github.com>
2019-10-04 11:55:06 -0300

    Add files via upload

--- HoughTrans.py
+++ HoughTrans.py
@@ -1,6 +1,8 @@
-import cv2
 import numpy as np
+import cv2
 from Converters import *
+from math import radians, cos, sin
+import numpy
 
 
 
@@ -10,9 +12,13 @@ class HoughTrans:
         self.img = img
         self.rmin = rmin
         self.rmax = rmax
+        self.limSupD = 60
+        self.limSupE = 120
+        self.limInfE = 240
+        self.limInfD = 300
         self.width = img.shape[0]
         self.height = img.shape[1]
-        self.point = []
+        self.px_on = np.empty(shape=[])
         #self.__acumalador = np.empty(shape=[img.shape[0], img.shape[1], (self.rmax - self.rmin)])  # aloca uma matriz [][][]
         self.Hough(img, rmin, rmax)
 
@@ -20,8 +26,8 @@ class HoughTrans:
 
     def Hough(self, img, rmin, rmax):
         self.__allocateAcumulador(self.height, self.width)  # criando acumulaador apartir da altura e largura
-        self.point = Converters.BufferedImageToPoint(self, img, 0)
-
+        self.px_on = Converters.BufferedImageToPoint(self, img, 0)
+        #self.applyMethod(10,10,10)
 
     def __allocateAcumulador(self, lmax, cmax): # cria um acumulador
         try:
@@ -29,7 +35,7 @@ class HoughTrans:
             self.__acumalador = np.empty(shape=[lmax, cmax, (self.rmax - self.rmin)])  # aloca uma matriz [][][],
             for linha in range(0, lmax-1):
                 for coluna in range(0, cmax-1):
-                    self.__acumalador[linha][coluna] = (self.rmax - self.rmin) #(self.rmax - self.rmin)
+                    self.__acumalador[linha][coluna] = np.empty(shape=[self.rmax - self.rmin]) #(self.rmax - self.rmin)
             #print('alocado')
 
             #print('inicializando...')
@@ -42,7 +48,78 @@ class HoughTrans:
         except:
             print("Hough Transform: Erro na inicializa do acumulador.")
 
+    def applyMethod(self, qtd, perc, fator):
+
+            #colocar execoes
+
+        for raio in range(0, (self.rmax - self.rmin)):
+
+            #circulo = numpy.empty(shape=[])
+            circulo = numpy.empty(shape=[self.getCirculo((raio + self.rmin), perc, fator)])
+
+            for np in range(0, len(self.px_on)-1):
+                for coord in range(0, len(circulo)):
+                    h_x = (self.pxs_on[np].x + circulo[coord].x)
+                    h_y = (self.pxs_on[np].y + circulo[coord].y)
+                    if((h_x >= 0 and h_x < self.width) and (h_y >= 0 and h_y < self.height)):
+                        self.__acumalador[h_x][h_y][raio] += 1
+            return self.getPeak(qtd)
+
+    def getCirculo(self, raio, perc, fator):
+        circulo = np.empty(shape=[1]) #
+        #max_div = (fator*raio)
+        Xant = 0
+        Yant = 0
 
 
+        for passo in range(0, 360):
+            if((passo > 0 and passo < self.limSupD) or (passo > self.limInfD and passo < 360) or (passo > self.limSupE and passo < self.limInfE)):
+                angulo = radians(passo)
+                Xc = round(raio * perc * cos(angulo))
+                Yc = round(raio * perc * sin(angulo))
+                if((passo == 0) or (Xc != Xant) or (Yc != Yant)):
+                    Xant = Xc
+                    Yant = Yc
+                    circulo[0] = np.empty(shape=[Xc, Yc])
+            return circulo
 
 
+    def getPeak(self, qtd):
+        coord_center = np.empty(shape=[qtd,4])
+        index = 0
+        cont = 0
+        trocou = True
+        for raio in range(0, (self.rmax - self.rmin)):
+            for lin in range(0, len(self.__acumalador)):
+                for col in range(0, len(self.__acumalador[0])):
+                    if(cont < len(coord_center)):
+                        coord_center[cont][0] = lin
+                        coord_center[cont][1] = col
+                        coord_center[cont][2] = raio + self.rmin
+                        coord_center[cont][3] = self.__acumalador[lin][col][raio]
+                        cont += 1
+                    else:
+                        if(trocou == True):
+                            index = self.index_Min(coord_center)
+                            trocou = False
+                        if(self.__acumalador[lin][col][raio] > coord_center[index][3]):
+                            coord_center[index][0] = lin
+                            coord_center[index][1] = col
+                            coord_center[index][2] = raio + self.rmin
+                            coord_center[index][3] = self.__accumulator[lin][col][raio]
+                            trocou = True
+        return coord_center
+
+
+    def index_Min(self, matriz):
+        min = 100000000
+        index = 0
+        try:
+            for lin in range(0, len(matriz)):
+                if(min > matriz[lin][3]):
+                    min = matriz[lin][3]
+                    index = lin
+        except Exception as erro:
+            print(f"Matriz sem memoria na funcao Index_min classe Hough Transform, erro {erro.__class__}")
+
+        return index
--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from HoughTrans import *
 import cv2
 
-img = cv2.imread("C:/Users/Brunno Santos/Downloads/HOUGH/4Pixel.png")
-#print(img.shape[0])
-ob = HoughTrans(img, 2, 6) # raio minimo e raio maximo
+img = cv2.imread('2.png')
+
+'''cv2.imshow('imagem', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+#‭50.325‬
+test = HoughTrans(img, 2, 6)
+# 275 x 183
+#138 x 91
+#194
+i = test.applyMethod(194,2,3)
+print(i)
+maior = i[0][0]
+maiorl = 0
+maiorc = 0
+'''for l in range(0, len(i[:][:])):
+    for c in range(0, len(i[:][:])):
+        if(maior < i[l][c]):
+            maior = i[l][c]
+            maiorl = l
+            maiorc = c
+print(maior, maiorl, maiorc)
+print(i)'''
+
+
+
+
+
+
